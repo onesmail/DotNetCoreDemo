b98438513735a1a943201f5791531b84b40a8ef7
oneSmail <332059976@qq.com>
2024-05-12 21:10:53 +0800

    #

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,15 +1,41 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    // 自定义 JSON 序列化设置
+                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                    options.JsonSerializerOptions.IgnoreNullValues = true;
+                });
+
+// 配置 CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder => builder.WithOrigins("http://example.com"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddMemoryCache();
+builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {       
+                    options.Authority = "XXXXXXXXXXXXXXXXXXXXXX";
+                    options.RequireHttpsMetadata = false;
+                    options.Audience = "api1";
+                });
+
+
+
 
 var app = builder.Build();
+
+app.UseCors("AllowSpecificOrigin");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,6 +43,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
